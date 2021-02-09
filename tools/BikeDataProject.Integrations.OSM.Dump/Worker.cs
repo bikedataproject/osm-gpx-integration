a1b7cbbd45e429a4990b771cf6a538fde6ba9cfc
xivk <ben@itinero.tech>
2021-02-09 18:26:02 +0100

    Updated dump worker.

--- tools/BikeDataProject.Integrations.OSM.Dump/Worker.cs
+++ tools/BikeDataProject.Integrations.OSM.Dump/Worker.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using BikeDataProject.Integrations.OSM.Db;
+using FlatGeobuf.NTS;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -55,133 +56,109 @@ namespace BikeDataProject.Integrations.OSM.Dump
         {
             try
             {
-                var maxTrackCount = _configuration.GetValueOrDefault("MaxFeatures", int.MaxValue);
-                var tracks = _db.Tracks.Where(x => x.UserId != null)
-                    .Take(maxTrackCount);
-
-                var features = new List<Feature>();
-                var trackCount = 0;
-                foreach (var track in tracks)
+                IEnumerable<Feature> GetFeatures()
                 {
-                    trackCount++;
-                    if (stoppingToken.IsCancellationRequested) break;
-                    if (track.GpxFile == null) continue;
+                    var maxTrackCount = _configuration.GetValueOrDefault("MaxFeatures", int.MaxValue);
+                    var tracks = _db.Tracks.Where(x => x.Id < maxTrackCount)
+                        .Where(x => x.UserId != null).OrderBy(x => x.Id);
 
-                    _logger.LogInformation(
-                        $"Track {trackCount + 1}/{maxTrackCount} {track.Id}: {track.GpxFile?.Length} bytes");
+                    var trackCount = 0;
+                    foreach (var track in tracks) {
+                        trackCount++;
+                        if (stoppingToken.IsCancellationRequested) break;
+                        if (track.GpxFile == null) continue;
 
-                    try
-                    {
+                        _logger.LogInformation(
+                            $"Track {trackCount + 1}/{maxTrackCount} {track.Id}: {track.GpxFile?.Length} bytes");
+
                         // try compressed.
                         string? xml = null;
-                        try
-                        {
-                            await using (var memoryStream = new MemoryStream(track.GpxFile))
-                            await using (var gzipStream1 = new GZipStream(memoryStream, CompressionMode.Decompress))
-                            await using (var gzipStream2 = new GZipStream(gzipStream1, CompressionMode.Decompress))
-                            using (var streamReader = new StreamReader(gzipStream2))
-                            {
-                                xml = await streamReader.ReadToEndAsync();
+                        try {
+                            using (var memoryStream = new MemoryStream(track.GpxFile))
+                            using (var gzipStream1 = new GZipStream(memoryStream, CompressionMode.Decompress))
+                            using (var gzipStream2 = new GZipStream(gzipStream1, CompressionMode.Decompress))
+                            using (var streamReader = new StreamReader(gzipStream2)) {
+                                xml = streamReader.ReadToEnd();
                             }
                         }
-                        catch (Exception e)
-                        {
+                        catch (Exception e) {
 
                         }
 
                         // try uncompressed.
-                        if (string.IsNullOrWhiteSpace(xml))
-                        {
-                            try
-                            {
-                                await using (var memoryStream = new MemoryStream(track.GpxFile))
-                                await using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
-                                using (var streamReader = new StreamReader(gzipStream))
-                                {
-                                    xml = await streamReader.ReadToEndAsync();
+                        if (string.IsNullOrWhiteSpace(xml)) {
+                            try {
+                                using (var memoryStream = new MemoryStream(track.GpxFile))
+                                using (var gzipStream =
+                                    new GZipStream(memoryStream, CompressionMode.Decompress))
+                                using (var streamReader = new StreamReader(gzipStream)) {
+                                    xml = streamReader.ReadToEnd();
                                 }
                             }
-                            catch (Exception e)
-                            {
+                            catch (Exception e) {
 
                             }
                         }
 
                         // read gpx.
                         Feature[]? trackFeatures = null;
-                        if (!string.IsNullOrWhiteSpace(xml))
-                        {
-                            using (var reader = new StringReader(xml))
-                            using (var xmlReader = XmlReader.Create(reader))
-                            {
-                                (_, trackFeatures, _) =
-                                    GpxReader.ReadFeatures(xmlReader, new GpxReaderSettings()
-                                    {
-                                        DefaultCreatorIfMissing = "OSM"
-                                    }, GeometryFactory.Default);
+                        try {
+                            if (!string.IsNullOrWhiteSpace(xml)) {
+                                using (var reader = new StringReader(xml))
+                                using (var xmlReader = XmlReader.Create(reader)) {
+                                    (_, trackFeatures, _) =
+                                        GpxReader.ReadFeatures(xmlReader, new GpxReaderSettings()
+                                        {
+                                            DefaultCreatorIfMissing = "OSM"
+                                        }, GeometryFactory.Default);
+                                }
                             }
                         }
+                        catch (Exception e) {
+                            _logger.LogError(e, "Unhandled exception while parsing GPX");
+                        }
 
-                        if (trackFeatures != null)
-                        {
-                            foreach (var tf in trackFeatures)
-                            {
+                        if (trackFeatures != null) {
+                            foreach (var tf in trackFeatures) {
                                 if (stoppingToken.IsCancellationRequested) break;
                                 if (tf.Geometry is Point) continue;
                                 if (tf.Geometry is Polygon) continue;
 
-                                if (tf.Geometry is MultiLineString mls)
-                                {
-                                    foreach (var g in mls)
-                                    {
-                                        if (g is LineString ls)
-                                        {
-                                            if (ls.Count >= 2)
-                                            {
-                                                features.Add(new Feature(ls, new AttributesTable()
+                                if (tf.Geometry is MultiLineString mls) {
+                                    foreach (var g in mls) {
+                                        if (g is LineString ls) {
+                                            if (ls.Count >= 2) {
+                                                yield return new Feature(ls, new AttributesTable()
                                                 {
                                                     {"track_id", track.Id}
-                                                }));
+                                                });
                                             }
                                         }
                                     }
                                 }
-                                else if (tf.Geometry is LineString ls)
-                                {
-                                    if (ls.Count >= 2)
-                                    {
-                                        features.Add(new Feature(tf.Geometry, new AttributesTable()
+                                else if (tf.Geometry is LineString ls) {
+                                    if (ls.Count >= 2) {
+                                        yield return new Feature(tf.Geometry, new AttributesTable()
                                         {
                                             {"track_id", track.Id}
-                                        }));
+                                        });
                                     }
                                 }
                             }
                         }
-                        else
-                        {
+                        else {
                             _logger.LogWarning("Failed to parse track: {trackId} - {trackFileName}", track.Id,
                                 track.GpxFileName);
                         }
                     }
-                    catch (Exception e)
-                    {
-                        _logger.LogError(e, "Unhandled exception during track parsing");
-                    }
-
-                    _logger.LogInformation("Features {features}", features.Count);
                 }
 
-                _logger.LogInformation("Writing shapefile...");
+                var outputFile = this._configuration.GetValueOrDefault("OutputFile", "osm-gpx.fbg");
+                _logger.LogDebug("Building output file: {outputFile}", outputFile);
+                using var outputFileStream = File.Open(outputFile, FileMode.Create);
+                FeatureCollectionConversions.Serialize(outputFileStream, GetFeatures(), FlatGeobuf.GeometryType.LineString);
                 
-                var header = ShapefileDataWriter.GetHeader(features[0], features.Count);
-                var outputShape = this._configuration.GetValueOrDefault("OutputFile", "shapefile");
-                var shape = new ShapefileDataWriter(outputShape, GeometryFactory.Default)
-                {
-                    Header = header
-                };
-                
-                shape.Write(features);
+                _logger.LogInformation("Built output file: {outputFile}", outputFile);
             }
             catch (Exception e)
             {
